3a6a176fa26a9ea51afb5be474b57710626c97b7
zombiemurman <zombiemurman@yandex.ru>
2026-04-03 13:44:15 +0300

    L9

--- Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
+++ Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 
 namespace Assets._Project.Develop.Editor
@@ -6,14 +7,46 @@ namespace Assets._Project.Develop.Editor
     [InitializeOnLoad]
     public static class EntryPointSceneAutoLoader
     {
+        private const string PlayFromBootsrapKey = "PlayFromBootstrapKey";
+        private const string MenuPath = "PlayFromBootstrap/Enabled";
+
         static EntryPointSceneAutoLoader()
         {
-            if (EditorBuildSettings.scenes.Length == 0)
-                return;
-
-            EditorSceneManager.playModeStartScene = AssetDatabase
-                .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
         }
 
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if(state == PlayModeStateChange.ExitingEditMode)
+            {
+                if(EditorPrefs.GetBool(PlayFromBootsrapKey) == false)
+                {
+                    EditorSceneManager.playModeStartScene = null;
+                    return;
+                }
+
+                if(EditorBuildSettings.scenes.Length == 0)
+                    return;
+
+                EditorSceneManager.playModeStartScene = AssetDatabase
+                   .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+            }
+        }
+
+        [MenuItem(MenuPath)]
+        private static void Toggle()
+        {
+            bool result = EditorPrefs.GetBool(PlayFromBootsrapKey);
+
+            EditorPrefs.SetBool(PlayFromBootsrapKey, !result);
+        }
+
+        [MenuItem(MenuPath, true)]
+        private static bool ToggleValidate()
+        {
+            Menu.SetChecked(MenuPath, EditorPrefs.GetBool(PlayFromBootsrapKey));
+
+            return true;
+        }
     }
 }
